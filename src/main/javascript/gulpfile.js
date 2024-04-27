45a103116bd1dc8c31ea31bcb227cc10dad02aff
Paulo Rohling <paulohr.freecopias@gmail.com>
2024-04-27 10:37:35 -0300

    feat: configuring prettier

--- src/main/javascript/gulpfile.js
+++ src/main/javascript/gulpfile.js
@@ -9,34 +9,30 @@ const uglifycss = require("gulp-uglifycss");
 const concat = require("gulp-concat");
 const webpack = require("webpack-stream");
 const compiler = require("webpack");
-const webpackConfig = require('./webpack.config.js');
+const webpackConfig = require("./webpack.config.js");
+const sassCompiler = gulpSass(sass);
 
-const sassCompiler = gulpSass(sass)
+const DIST_PATH = "./../resources/static/dist";
 
 gulp.task("sass", () => {
-  return gulp.src("./scss/**/*.scss")
-  .pipe(sassCompiler({outputStyle: "compressed"}).on("error",
-      sassCompiler.logError))
-  .pipe(postcss([tailwindcss, tailwindcssForms]))
-  .pipe(uglifycss())
-  .pipe(concat("styles.css"))
-  .pipe(gulp.dest("./../resources/static/dist"));
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sassCompiler({ outputStyle: "compressed" }).on("error", sassCompiler.logError))
+    .pipe(postcss([tailwindcss, tailwindcssForms]))
+    .pipe(uglifycss())
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest(DIST_PATH));
 });
 
 gulp.task("webpack:build", () => {
-  return gulp.src("./app/index.js")
-  .pipe(webpack(webpackConfig, compiler))
-  .pipe(gulp.dest("./../resources/static/dist"));
+  return gulp.src("./app/index.js").pipe(webpack(webpackConfig, compiler)).pipe(gulp.dest(DIST_PATH));
 });
 
 gulp.task("build", gulp.parallel("sass", "webpack:build"));
 
 gulp.task("watch", () => {
-  const watcher = watch(["./app/**/*.js", "./scss/**/*.scss",
-    "./../resources/templates/**/*.html"]);
-  watcher.on('change', gulp.series("build"));
+  const watcher = watch(["./app/**/*.js", "./scss/**/*.scss", "./../resources/templates/**/*.html"]);
+  watcher.on("change", gulp.series("build"));
 });
 
-gulp.task('default', gulp.parallel("build", "watch"));
-
-// gulp.task("default", gulp.series("sass"));
+gulp.task("default", gulp.parallel("build", "watch"));
